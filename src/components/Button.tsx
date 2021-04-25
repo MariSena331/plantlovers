253dbae9dec4455aae9849630a2871f7a792c80b
MariSena331 <mariana7007@hotmail.com>
2021-04-25 10:32:45 -0300

    Inserindo navegação e telas de confirmação e identificacao de usuario
    Signed-off-by: MariSena331 <mariana7007@hotmail.com>

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+
 import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+
 import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 interface ButtonProps extends TouchableOpacityProps {
     title: string;
@@ -9,11 +12,11 @@ interface ButtonProps extends TouchableOpacityProps {
 export function Button({title, ...rest}: ButtonProps) {
     return (
         <TouchableOpacity
-            style={styles.button}
+            style={styles.container}
             activeOpacity={0.8}
             {...rest}
         >
-            <Text style={styles.buttonText}>
+            <Text style={styles.text}>
                 {title}
             </Text>
         </TouchableOpacity>
@@ -21,17 +24,16 @@ export function Button({title, ...rest}: ButtonProps) {
 }
 
 const styles = StyleSheet.create({
-    button: {
+    container: {
         backgroundColor: colors.green,
+        height: 56,
+        borderRadius: 16,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
-        height: 56,
-        paddingHorizontal: 10,
     },
-    buttonText: {
+    text: {
         color: colors.white,
-        fontSize: 18,
+        fontSize: 16,
+        fontFamily: fonts.heading
     }
 })
